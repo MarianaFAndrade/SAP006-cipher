ee838e416c6224373950ba910b9038299e58988a
Mariana Andrade <andrade.marianaferreira@gmail.com>
2021-06-13 16:32:46 -0300

    Incluindo a Cifra finalizada e o DOM

--- src/cipher.js
+++ src/cipher.js
@@ -1,8 +1,39 @@
 const cipher = {
-  // ...
+  encode: function (chaveDeDesloc, string){
+    let entradaEncode = string.toUpperCase();
+    let cifrarMensagem = '';
+
+    for (let i = 0; i < entradaEncode.length; i++){
+      
+      let codigoAsc = entradaEncode[i].charCodeAt();
+      // console.log('codigoAsc:', codigoAsc);
+
+      let encondeForm = ((codigoAsc - 65 + chaveDeDesloc) % 26) + 65;
+      // console.log('encondeForm:', encondeForm);
+      
+      cifrarMensagem += String.fromCharCode(encondeForm);
+    }
+    return cifrarMensagem;
+  },
+
+
+  decode: function (chaveDeslocamentoDecode, string){
+    let entradaDecode = string.toUpperCase();
+    let decifraMensagem = '';
+
+    for (let i = 0; i < entradaDecode.length; i++){
+      
+      let codigoAscDecipher = entradaDecode[i].charCodeAt();
+      console.log('codigoAscDecipher:', codigoAscDecipher);
+
+      let decodeForm = ((codigoAscDecipher - 65 - chaveDeslocamentoDecode) % 26) + 65;
+      console.log('decodeForm:', decodeForm);
+
+      decifraMensagem += String.fromCharCode(decodeForm);
+
+    }
+    return decifraMensagem;
+  }
 };
 
 export default cipher;
-
-
-
--- src/index.js
+++ src/index.js
@@ -1,4 +1,41 @@
 import cipher from './cipher.js';
 
-console.log(cipher);
+const mensagem = document.getElementById('textoInserido1')
+const chaveDeslocamentoEncode = document.getElementById('seletor1')
+const botao = document.getElementById('botaoCodificar')
+const resultado = document.getElementById('textoRecebido1')
 
+
+botao.addEventListener('click', () => {
+    let string = mensagem.value
+    let seletor = parseInt(chaveDeslocamentoEncode.value)
+
+    console.log(mensagem.value)
+    console.log(chaveDeslocamentoEncode.value)
+    console.log(resultado.value)
+
+    const mensagemCodificada = cipher.encode(seletor, string)
+    console.log('chaveDeslocamentoEncode:', mensagemCodificada)
+
+    resultado.value = mensagemCodificada
+})
+
+const mensagemDecode = document.getElementById('textoInserido2')
+const chaveDeslocamentoDecode = document.getElementById('seletor2')
+const botaoDecode = document.getElementById('botaoDecodificar')
+const resultadoDecode = document.getElementById('textoRecebido2')
+
+
+botaoDecode.addEventListener('click', () => {
+    let stringDecode = mensagemDecode.value
+    let seletorDecode = parseInt(chaveDeslocamentoDecode.value)
+
+    console.log(mensagemDecode.value)
+    console.log(chaveDeslocamentoDecode.value)
+    console.log(resultadoDecode.value)
+
+    const mensagemDecodificada = cipher.decode(seletorDecode, stringDecode)
+    console.log('chaveDeslocamentoDecode:', mensagemDecodificada)
+
+    resultadoDecode.value = mensagemDecodificada
+})
